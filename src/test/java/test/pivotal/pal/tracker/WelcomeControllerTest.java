048543e4a45e58d46cc8e62ea55379283487ed70
Alexis Johnson <alexis.johnson052@gmail.com>
2018-11-01 08:41:37 -0400

    manage environment variables and create a manifest.yml

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -3,14 +3,14 @@ package test.pivotal.pal.tracker;
 import io.pivotal.pal.tracker.WelcomeController;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WelcomeControllerTest {
 
     @Test
     public void itSaysHello() throws Exception {
-        WelcomeController controller = new WelcomeController();
+        WelcomeController controller = new WelcomeController("A welcome message");
 
-        assertEquals( "hello", controller.sayHello());
+        assertThat(controller.sayHello()).isEqualTo("A welcome message");
     }
 }
